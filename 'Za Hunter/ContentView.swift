ae4aa1edda7e9b043124f47d4437369c6abc9754
Ali Feras <ferasali876@gmail.com>
2022-08-01 02:38:39 -0500

    Added map search feature

--- 'Za Hunter/ContentView.swift
+++ 'Za Hunter/ContentView.swift
@@ -10,28 +10,57 @@ import MapKit
 
 
 struct ContentView: View {
+    @State private var places = [Place]()
     @StateObject var locationManager = LocationManager()
     @State private var userTrackingMode: MapUserTrackingMode = .follow
     @State private var region = MKCoordinateRegion(
-    center: CLLocationCoordinate2D(
-        latitude: 42.0558,
-        longitude: -87.6743),
-    span: MKCoordinateSpan(
-        latitudeDelta: 0.5,
-        longitudeDelta: 0.5)
+        center: CLLocationCoordinate2D(
+            latitude: 42.0558,
+            longitude: -87.6743),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.5,
+            longitudeDelta: 0.5)
     )
+    
     var body: some View {
-        Map(
-            coordinateRegion: $region,
+        Map(coordinateRegion: $region,
             interactionModes: .all,
             showsUserLocation: true,
-            userTrackingMode: $userTrackingMode
-        )
+            userTrackingMode: $userTrackingMode,
+            annotationItems: places) { place in
+            MapPin(coordinate: place.annotation.coordinate)
+        }
+            .onAppear() {
+            performSearch(item: "Pizza")
     }
 }
+    func performSearch(item: String) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = item
+        searchRequest.region = region
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { (response, error) in
+            if let response = response {
+                for mapItem in response.mapItems {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    annotation.title = mapItem.name
+                    places.append(Place(annotation: annotation, mapItem: mapItem))
+                    }
+                }
+            }
+        }
+    }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        }
     }
-}
+
+struct Place: Identifiable {
+    let id = UUID()
+    let annotation: MKPointAnnotation
+    let mapItem: MKMapItem
+    }
+
